e53e1d2ce8eb9890d8f871715dc84f82ece2b938
Yovez <36488607+Yovez@users.noreply.github.com>
2021-05-04 19:40:31 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var fetch = require('node-fetch');
+var async = require('async');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var zipcode = 90210;
+  if (req.cookies.zipcode) {
+    zipcode = req.cookies.zipcode;
+  }
+  if (req.query.zipcode) {
+    zipcode = req.query.zipcode;
+    res.cookie('zipcode', zipcode, { httpOnly: true, encode: String });
+  }
+  async.series(
+    [
+      (callback) => {
+        let url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + ',us&units=imperial&appid=f0e9be06b2eace17712b103c03cdc6d4';
+        try {
+          fetch(url)
+            .then(res => res.json())
+            .then((json) => {
+              return callback(null, json);
+            });
+        } catch (e) {
+          return callback(err, response);
+        }
+      }
+    ],
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        return res.render('index', { error: err });
+      }
+      if (results) {
+        console.log(results[0]);
+        if (results[0].cod === "404") {
+          return res.render('index', { message: "The Zipcode of " + zipcode + " is not valid! Please enter a valid USA Zipcode." })
+        }
+        return res.render('index', { weather: results[0] });
+      }
+    }
+  );
+
+});
+
+module.exports = router;
