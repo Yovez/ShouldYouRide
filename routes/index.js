82eeda6944da1e54d42020a8a363d34fb8469755
Yovez <36488607+Yovez@users.noreply.github.com>
2021-05-07 20:42:12 -0400

    Lat and Long changes

--- routes/index.js
+++ routes/index.js
@@ -6,32 +6,16 @@ const { Navigator } = require('node-navigator');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var lat = 0, lon = 0;
-  if (req.cookies.lat) {
-    lat = req.cookies.lat;
-  }
-  if (req.cookies.lon) {
-    lon = req.cookies.lon;
-  }
-  if (req.query.lat) {
-    lat = req.query.lat;
-    res.cookie('lat', lat, { httpOnly: true, encode: String });
-  }
-  if (req.query.lon) {
-    lon = req.query.lon;
-    res.cookie('lon', lon, { httpOnly: true, encode: String });
-  }
+  var lat = 0;
+  var lon = 0;
   const nav = new Navigator();
-  if (nav.geolocation) {
+  if ('geolocation' in nav) {
     nav.geolocation.getCurrentPosition((success, error) => {
-      if (error) {
-        console.log(error);
-        //return res.render('index', { message: "Please enable location to use this website!", error: error });
-      }
-      if (lat === 0)
+      if (error) console.log(error);
+      if (success) {
         lat = success.latitude;
-      if (lon === 0)
         lon = success.longitude;
+      }
     });
   }
   console.log("Lat: " + lat + " - Lon: " + lon);
