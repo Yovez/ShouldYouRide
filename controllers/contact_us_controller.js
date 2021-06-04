38c73fc140ed056d5305661fb9f0d60ce26a5cdc
Yovez <36488607+Yovez@users.noreply.github.com>
2021-06-04 18:27:11 -0400

    Revamp Contact Us UI/UX

--- controllers/contact_us_controller.js
+++ controllers/contact_us_controller.js
@@ -6,14 +6,14 @@ exports.contactUs_get = (req, res, next) => {
 }
 
 exports.contactUs_post = [
-    body('email').trim().isEmail().withMessage("Email is cannot be left empty.").escape(),
+    body('email').trim().isEmail().withMessage("Email cannot be empty.").trim(),
     body('feedback').isLength({ min: 10 }).withMessage("Feedback must be at least 10 characters long.").escape(),
 
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             // There are errors
-            res.render('contactus', { page: "contactus", errors: errors.array() });
+            res.render('contactus', { page: "contactus", email: req.body.email, feedback: req.body.feedback, errors: errors.mapped() });
         } else {
             res.render('contactus', { page: "contactus", success: true });
         }
