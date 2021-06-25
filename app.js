8d8432bde767a317db44ecca3dc46c67717fa6cd
Yovez <36488607+Yovez@users.noreply.github.com>
2021-06-25 17:33:45 -0400

    Reimplement mongoose

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
+var contactUsRouter = require('./routes/contactUs');
+var userRouter = require('./routes/user');
 
 var app = express();
 
@@ -19,6 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', contactUsRouter);
+app.use('/', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -35,5 +40,12 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var mongoDB = 'mongodb+srv://yovez:7DaB8u6agqg406aq@cluster0.u6hxz.mongodb.net/should_you_ride?retryWrites=true&w=majority'
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 module.exports = app;
